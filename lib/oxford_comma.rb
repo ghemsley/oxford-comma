e2a2dcb6d2fa41aa699973b5df4b0ccfb29f3970
ghemsley <ghemsley@protonmail.ch>
2020-10-22 10:00:44 -0500

    commit tree 3c0de7a7a38e88e0a4821da9e1d6a88c827d4599

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    all_but_last_word_array = array[0..-2]
+    last_word = 'and ' + array[-1]
+    all_words_array = all_but_last_word_array.push(last_word)
+    all_words_array = all_words_array.join(', ')
+  elsif array.length == 2
+    last_word = 'and ' + array.pop
+    all_words_array = array.push(last_word)
+   all_words_array = all_words_array.join(' ')
+  else
+    all_words_array = array[0]
+  end
+  return all_words_array
 end
